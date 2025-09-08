f233e1f29107d98c96d9636fd18a8b96443fb4e6
Jeshaiah73 <jedrac46@gmail.com>
2025-09-08 22:10:27 +0700

    SortSelector - Working With API CoinGecko

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 
@@ -35,7 +36,25 @@ const App = () => {
     return (
       coin.name.toLowerCase().includes(filter.toLowerCase()) || coin.symbol.toLowerCase().includes(filter.toLowerCase())
     );
+  })
+  .slice()
+  .sort((a, b) => {
+    switch (sortBy) {
+      case 'market_cap_desc':
+        return b.market_cap - a.market_cap;
+      case 'market_cap_asc':
+        return a.market_cap - b.market_cap;
+      case 'price_desc':
+        return b.current_price - a.current_price;
+      case 'price_asc':
+        return a.current_price - b.current_price;
+      case 'change_desc':
+        return b.price_change_percentage_24h - a.price_change_percentage_24h;
+      case 'change_asc':
+        return a.price_change_percentage_24h - b.price_change_percentage_24h;
+    }
   });
+
 
   return (
     <div>
@@ -46,6 +65,7 @@ const App = () => {
       <div className="top-controls">
         <FilterInput filter={filter} onFilterChange={setFilter} />
         <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
       </div>
 
       {!loading && !error && (
