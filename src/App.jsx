e2e8ded1dcc8964221412f0e625751a8ead1c739
Jeshaiah73 <jedrac46@gmail.com>
2025-09-08 19:46:36 +0700

    Display Coin Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
 const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => { 
+    const fetchCoins = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw new Error('Failed to fetch data'); 
+        const data = await response.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCoins();
+  }, []);
+
   return (
     <div>
       <h1>Lightcrypt Dash</h1>
+      { loading && <p>Loading...</p>}
+      { error && <div className='error'>{error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          { coins.map(coin => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}      
+        </main> 
+        ) }
     </div>
   );
-}
+};
 
 export default App;
