3edaaa97d661b1077d21dc97e8ac1a21891ebb0b
Jeshaiah73 <jedrac46@gmail.com>
2025-09-09 09:51:26 +0700

    NotFoundPage - 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/home';
 import {Routes, Route} from 'react-router';
 import AboutPage from "./pages/about";
 import Header from "./components/Header";
+import NotFoundPage from "./pages/not-found";
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 
@@ -50,6 +51,7 @@ const App = () => {
     } 
       />
       <Route path='/about' element={<AboutPage /> } />
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
     </>
   );
